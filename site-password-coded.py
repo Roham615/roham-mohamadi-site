106f9e5ac825c41323e3a8c131f8f6fdf20f5408
Roham <rohi.mohi1243@gmail.com>
2026-09-12 18:42:54 +0330

    Create site-password-coded.py

---
+++ site-password-coded.py
@@ -0,0 +1,508 @@
+# -*- coding: utf-8 -*-
+
+_0x91 = [
+    87,104,97,116,32,97,114,101,32,121,111,117,32,116,114,121,
+    105,110,103,32,116,111,32,114,101,97,99,104,63
+]
+
+_0x92 = [
+    67,111,110,116,97,99,116,32,109,101,32,58,32,114,111,104,97,
+    109,46,100,101,110,116,101,64,103,109,97,105,108,46,99,111,109
+]
+
+def _a(x):
+    return ''.join(chr(i) for i in x)
+
+def _b(x):
+    y = []
+    for i in range(len(x)):
+        y.append(x[i])
+    return y
+
+def _c(x):
+    z = ""
+    for i in x:
+        z += i
+    return z
+
+def _d(x):
+    return _c(_b(x))
+
+def _e(x):
+    return _d(_a(x))
+
+def _f(x):
+    a = _e(x)
+    return ''.join([c for c in a])
+
+def _g(x):
+    a = []
+    for c in x:
+        a.append(ord(c))
+    return a
+
+def _h(x):
+    return _f(x)
+
+def _i(x):
+    return _h(x)
+
+def _j(x):
+    return _i(x)
+
+def _k(x):
+    return _j(x)
+
+def _l(x):
+    return _k(x)
+
+def _m(x):
+    return _l(x)
+
+def _n(x):
+    return _m(x)
+
+def _o(x):
+    return _n(x)
+
+def _p(x):
+    return _o(x)
+
+def _q(x):
+    return _p(x)
+
+def _r(x):
+    return _q(x)
+
+def _s(x):
+    return _r(x)
+
+def _t(x):
+    return _s(x)
+
+def _u(x):
+    return _t(x)
+
+def _v(x):
+    return _u(x)
+
+def _w(x):
+    return _v(x)
+
+def _x(x):
+    return _w(x)
+
+def _y(x):
+    return _x(x)
+
+def _z(x):
+    return _y(x)
+
+def _aa(x):
+    return _z(x)
+
+def _ab(x):
+    return _aa(x)
+
+def _ac(x):
+    return _ab(x)
+
+def _ad(x):
+    return _ac(x)
+
+def _ae(x):
+    return _ad(x)
+
+def _af(x):
+    return _ae(x)
+
+def _ag(x):
+    return _af(x)
+
+def _ah(x):
+    return _ag(x)
+
+def _ai(x):
+    return _ah(x)
+
+def _aj(x):
+    return _ai(x)
+
+def _ak(x):
+    return _aj(x)
+
+def _al(x):
+    return _ak(x)
+
+def _am(x):
+    return _al(x)
+
+def _an(x):
+    return _am(x)
+
+def _ao(x):
+    return _an(x)
+
+def _ap(x):
+    return _ao(x)
+
+def _aq(x):
+    return _ap(x)
+
+def _ar(x):
+    return _aq(x)
+
+def _as(x):
+    return _ar(x)
+
+def _at(x):
+    return _as(x)
+
+def _au(x):
+    return _at(x)
+
+def _av(x):
+    return _au(x)
+
+def _aw(x):
+    return _av(x)
+
+def _ax(x):
+    return _aw(x)
+
+def _ay(x):
+    return _ax(x)
+
+def _az(x):
+    return _ay(x)
+
+def _ba(x):
+    return _az(x)
+
+def _bb(x):
+    return _ba(x)
+
+def _bc(x):
+    return _bb(x)
+
+def _bd(x):
+    return _bc(x)
+
+def _be(x):
+    return _bd(x)
+
+def _bf(x):
+    return _be(x)
+
+def _bg(x):
+    return _bf(x)
+
+def _bh(x):
+    return _bg(x)
+
+def _bi(x):
+    return _bh(x)
+
+def _bj(x):
+    return _bi(x)
+
+def _bk(x):
+    return _bj(x)
+
+def _bl(x):
+    return _bk(x)
+
+def _bm(x):
+    return _bl(x)
+
+def _bn(x):
+    return _bm(x)
+
+def _bo(x):
+    return _bn(x)
+
+def _bp(x):
+    return _bo(x)
+
+def _bq(x):
+    return _bp(x)
+
+def _br(x):
+    return _bq(x)
+
+def _bs(x):
+    return _br(x)
+
+def _bt(x):
+    return _bs(x)
+
+def _bu(x):
+    return _bt(x)
+
+def _bv(x):
+    return _bu(x)
+
+def _bw(x):
+    return _bv(x)
+
+def _bx(x):
+    return _bw(x)
+
+def _by(x):
+    return _bx(x)
+
+def _bz(x):
+    return _by(x)
+
+def _ca(x):
+    return _bz(x)
+
+def _cb(x):
+    return _ca(x)
+
+def _cc(x):
+    return _cb(x)
+
+def _cd(x):
+    return _cc(x)
+
+def _ce(x):
+    return _cd(x)
+
+def _cf(x):
+    return _ce(x)
+
+def _cg(x):
+    return _cf(x)
+
+def _ch(x):
+    return _cg(x)
+
+def _ci(x):
+    return _ch(x)
+
+def _cj(x):
+    return _ci(x)
+
+def _ck(x):
+    return _cj(x)
+
+def _cl(x):
+    return _ck(x)
+
+def _cm(x):
+    return _cl(x)
+
+def _cn(x):
+    return _cm(x)
+
+def _co(x):
+    return _cn(x)
+
+def _cp(x):
+    return _co(x)
+
+def _cq(x):
+    return _cp(x)
+
+def _cr(x):
+    return _cq(x)
+
+def _cs(x):
+    return _cr(x)
+
+def _ct(x):
+    return _cs(x)
+
+def _cu(x):
+    return _ct(x)
+
+def _cv(x):
+    return _cu(x)
+
+def _cw(x):
+    return _cv(x)
+
+def _cx(x):
+    return _cw(x)
+
+def _cy(x):
+    return _cx(x)
+
+def _cz(x):
+    return _cy(x)
+
+
+def _decode_first():
+    return _cz(_0x91)
+
+
+def _decode_second():
+    return _cz(_0x92)
+
+
+def _noise_001():
+    return sum([1, 2, 3, 4, 5]) - 15
+
+
+def _noise_002():
+    return ''.join(chr(x) for x in [65, 66, 67])
+
+
+def _noise_003():
+    a = [7, 14, 21, 28]
+    return [x // 7 for x in a]
+
+
+def _noise_004():
+    return {str(x): x * x for x in range(5)}
+
+
+def _noise_005():
+    a = "obfuscation"
+    return a[::-1][::-1]
+  def _noise_006():
+    return all(x == x for x in range(100))
+
+
+def _noise_007():
+    return ''.join(["_", "x", "_"])
+
+
+def _noise_008():
+    n = 0
+    for i in range(20):
+        n += i
+    return n
+
+
+def _noise_009():
+    return bytes([65, 66, 67]).decode()
+
+
+def _noise_010():
+    return tuple(range(10))
+
+
+def _noise_011():
+    a = {"alpha": 1, "beta": 2}
+    return a.get("alpha")
+
+
+def _noise_012():
+    return ''.join(map(str, [1, 2, 3, 4, 5]))
+
+
+def _noise_013():
+    x = 123456789
+    return (x ^ x) + 1 - 1
+
+
+def _noise_014():
+    return len("this_is_completely_unnecessary")
+
+
+def _noise_015():
+    return [i for i in range(50) if i % 2 == 0]
+
+
+def _noise_016():
+    x = "abcdefghijklmnopqrstuvwxyz"
+    return x.upper().lower()
+
+
+def _noise_017():
+    return max([3, 1, 4, 1, 5, 9])
+
+
+def _noise_018():
+    return min([3, 1, 4, 1, 5, 9])
+
+
+def _noise_019():
+    return sum(range(101))
+
+
+def _noise_020():
+    return (lambda x: x)(None)
+
+
+_unused_values = [
+    _noise_001(),
+    _noise_002(),
+    _noise_003(),
+    _noise_004(),
+    _noise_005(),
+    _noise_006(),
+    _noise_007(),
+    _noise_008(),
+    _noise_009(),
+    _noise_010(),
+    _noise_011(),
+    _noise_012(),
+    _noise_013(),
+    _noise_014(),
+    _noise_015(),
+    _noise_016(),
+    _noise_017(),
+    _noise_018(),
+    _noise_019(),
+    _noise_020(),
+]
+
+
+class _Container:
+    def __init__(self):
+        self._data = {}
+        self._state = 0
+
+    def _push(self, k, v):
+        self._data[k] = v
+        self._state += 1
+
+    def _pull(self, k):
+        return self._data.get(k)
+
+    def _reset(self):
+        self._data.clear()
+        self._state = 0
+
+    def _status(self):
+        return self._state
+
+
+class _Processor:
+    def __init__(self):
+        self._container = _Container()
+
+    def prepare(self):
+        self._container._push("first", _0x91)
+        self._container._push("second", _0x92)
+        self._container._push("unused", _unused_values)
+
+    def process(self):
+        first = self._container._pull("first")
+        second = self._container._pull("second")
+        return _cz(first), _cz(second)
+
+    def finalize(self, values):
+        first, second = values
+        print(first)
+        print(second)
+
+
+def _main():
+    _processor = _Processor()
+    _processor.prepare()
+
+    _stage_01 = _processor.process()
+    _stage_02 = (_stage_01[0], _stage_01[1])
+    _stage_03 = list(_stage_02)
+    _stage_04 = tuple(_stage_03)
+
+    if len(_stage_04) == 2:
+        _processor.finalize(_stage_04)
+
+
+if __name__ == "__main__":
+    _main()
